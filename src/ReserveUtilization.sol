cb9ca7a92e2b77869da02b082b25a714a06aac96
CJ42 <cavallerajean@gmail.com>
2026-04-25 06:53:21 +0200

    chore(tmp): comment out temporarily `ReserveUtilization.sol`

--- src/ReserveUtilization.sol
+++ src/ReserveUtilization.sol
@@ -1,28 +1,30 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.10;
 
-import {ISpoke} from 'aave-v4/src/spoke/interfaces/ISpoke.sol';
+// import {ISpoke} from 'aave-v4/src/spoke/interfaces/ISpoke.sol';
 
-contract ReserveUtilization {
-  ISpoke public immutable SPOKE;
+// contract ReserveUtilization {
+//   ISpoke public immutable SPOKE;
 
-  constructor(address spoke) {
-    require(spoke != address(0), 'INVALID_SPOKE');
-    SPOKE = ISpoke(spoke);
-  }
+//   constructor(address spoke) {
+//     require(spoke != address(0), 'INVALID_SPOKE');
+//     SPOKE = ISpoke(spoke);
+//   }
 
-  function getReserveUtilization(uint256 reserveId)
-    external
-    view
-    returns (uint256 suppliedAssets, uint256 totalDebt, uint256 utilization)
-  {
-    suppliedAssets = SPOKE.getReserveSuppliedAssets(reserveId);
-    totalDebt = SPOKE.getReserveTotalDebt(reserveId);
+//   function getReserveUtilization(
+//     uint256 reserveId
+//   )
+//     external
+//     view
+//     returns (uint256 suppliedAssets, uint256 totalDebt, uint256 utilization)
+//   {
+//     suppliedAssets = SPOKE.getReserveSuppliedAssets(reserveId);
+//     totalDebt = SPOKE.getReserveTotalDebt(reserveId);
 
-    if (suppliedAssets == 0) {
-      utilization = 0;
-    } else {
-      utilization = (totalDebt * 1e18) / suppliedAssets;
-    }
-  }
-}
+//     if (suppliedAssets == 0) {
+//       utilization = 0;
+//     } else {
+//       utilization = (totalDebt * 1e18) / suppliedAssets;
+//     }
+//   }
+// }
